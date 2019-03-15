886ae1da2378e6b10bc8027f985d61719e2fff84
ram <ramkrishan@kontikilabs.com>
2019-03-16 01:50:45 +0530

    Updates

---
+++ precache-manifest.84af4127c79ef3f22c36216d8eafd0ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "467465296ce4ff57b1cf",
+    "url": "/React-calculator/static/js/runtime~main.46746529.js"
+  },
+  {
+    "revision": "39f561ccc8a19ab5ec95",
+    "url": "/React-calculator/static/js/main.39f561cc.chunk.js"
+  },
+  {
+    "revision": "0c519752e319f9173934",
+    "url": "/React-calculator/static/js/2.0c519752.chunk.js"
+  },
+  {
+    "revision": "39f561ccc8a19ab5ec95",
+    "url": "/React-calculator/static/css/main.a1684030.chunk.css"
+  },
+  {
+    "revision": "0c519752e319f9173934",
+    "url": "/React-calculator/static/css/2.685618e9.chunk.css"
+  },
+  {
+    "revision": "66f3d2985e9a0a9f8a6e21379f112bcc",
+    "url": "/React-calculator/index.html"
+  }
+];
